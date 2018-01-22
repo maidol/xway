f1fcbe477ce47d6af7338feb1b26247e06a33e8d
maifuquan <maifuquan@ciwong.com>
2018-01-22 16:12:19 +0800

    修改KeepAlive配置

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -30,7 +30,7 @@ func NewDo() (http.HandlerFunc, error) {
 		// Proxy: http.ProxyFromEnvironment,
 		DialContext: (&net.Dialer{
 			Timeout:   60 * time.Second,
-			KeepAlive: 120 * time.Second,
+			KeepAlive: 300 * time.Second,
 		}).DialContext,
 		ResponseHeaderTimeout: 60 * time.Second,
 		TLSHandshakeTimeout:   30 * time.Second,
